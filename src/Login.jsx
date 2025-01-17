3fcf1bdc23d290cae3b0b1fcb9835613bd6d0804
Tarun Tailor <taruntailor7@gmail.com>
2025-01-17 16:34:24 +0530

    feat: login api integrated using axios

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,22 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
+  const [emailId, setEmailid] = useState("akshay@gmail.com");
+  const [password, setPassword] = useState("Akshay@1234");
+
+  const handleLogin = async () => {
+    try {
+      const loggedInUser = await axios.post("http://localhost:5000/auth/login", {
+        "email": emailId,
+        "password": password,
+      }, { withCredentials: true }); // we have to pass withCredentials because in backend credentials is true then itwill set the cookies in frontend browser.
+      console.log('loggedInUser', loggedInUser);
+    } catch (error) {
+      console.log('Error while loging in: ', error);
+    }
+  }
+
   return (
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-xl">
@@ -11,8 +29,10 @@ const Login = () => {
               </div>
               <input
                 type="text"
+                value={emailId}
                 placeholder="Enter your email..."
                 className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailid(e.target.value)}
               />
             </label> 
             <label className="form-control w-full max-w-xs pb-4">
@@ -21,13 +41,15 @@ const Login = () => {
               </div>
               <input
                 type="text"
+                value={password}
                 placeholder="Enter your password..."
                 className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </label> 
           </div>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
           </div>
         </div>
       </div>
